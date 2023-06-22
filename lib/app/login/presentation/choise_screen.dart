41888123464e441bb5293f1fd98c7532516c06da
waleed se3fan <89372677+waleed-se3fan@users.noreply.github.com>
2023-06-22 06:01:27 +0300

    Update choise_screen.dart

--- lib/app/login/presentation/choise_screen.dart
+++ lib/app/login/presentation/choise_screen.dart
@@ -1,56 +1,108 @@
+import 'package:cord_2/app/home_assistent/home_assiyent_profile_layout/home_assistent.dart';
 import 'package:cord_2/app/layout/presentation/cord_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 import '../../../core/components/gradient_button.dart';
+import '../../../core/styles/colors.dart';
 import '../../../core/utils/navigator.dart';
+import '../login_cubit/login_cubit.dart';
+import '../login_cubit/login_states.dart';
 
-class ChoiceScreen extends StatefulWidget {
+class ChoiceScreen extends StatelessWidget {
   const ChoiceScreen({super.key});
 
   @override
-  State<ChoiceScreen> createState() => _ChoiceScreenState();
-}
-
-class _ChoiceScreenState extends State<ChoiceScreen> {
-  @override
-  void initState() {
-    super.initState();
-  }
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-          padding: const EdgeInsets.only(left: 9, right: 9, top: 5, bottom: 110),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset('assets/images/choise.png'),
-              const Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  'Continue as :',
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: BlocConsumer<LoginCubit, LoginState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          var cubit = LoginCubit.get(context);
+          return Scaffold(
+              body: Container(
+            padding:
+                const EdgeInsets.only(left: 9, right: 9, top: 30, bottom: 120),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset('assets/images/choise.png'),
+                const Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 5),
+                    child: Text(
+                      'Continue as :',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                  ),
                 ),
-              ),
-              GradientButton(
-                height: 45.h,
-                onPressed: ()async {
-                  navigateAndFinish(context, const CordLayout());
-                },
-                title: 'Patient',
-              ),
-              const SizedBox(
-                height: 1,
-              ),
-              GradientButton(
-                height: 45.h,
-                onPressed: () {
-                  navigateAndFinish(context, const CordLayout());
-                },
-                title: 'Assistant',
-              )
-            ],
-          ),
-        ));
+                Container(
+                  width: double.infinity,
+                  height: 45.h,
+                  decoration: BoxDecoration(
+                    // gradient: AppColors.kGradient,
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      overlayColor:
+                          MaterialStateProperty.all(HexColor('#7966FF')),
+                      onTap: () {
+                        navigateAndFinish(context, const CordLayout());
+                      },
+                      child: const Center(
+                        child: Text(
+                          'Patient',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 19,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 45.h,
+                  decoration: BoxDecoration(
+                    gradient: LoginCubit.x ? AppColors.kGradient : null,
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      overlayColor:
+                          MaterialStateProperty.all(HexColor('#7966FF')),
+                      onTap: () {
+                        LoginCubit.x = true;
+                        navigateAndFinish(context, const HomeAssistant());
+                      },
+                      child: const Center(
+                        child: Text(
+                          'Assistent',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 19,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ));
+        },
+      ),
+    );
   }
 }
